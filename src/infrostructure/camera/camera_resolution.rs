79f9a253483db46214f5c030e48b14489f892284
a-givertzman <a.givertzman@icloud.com>
2025-01-14 13:39:48 +0300

    Camera | CameraConf | CameraResolution

--- src/infrostructure/camera/camera_resolution.rs
+++ src/infrostructure/camera/camera_resolution.rs
@@ -1,6 +1,11 @@
+use serde::Deserialize;
+
 ///
 /// The resolution of the camera
+#[derive(Clone, Debug, Deserialize)]
 pub struct CameraResolution {
+    #[serde(alias = "w")]
     pub width: usize,
+    #[serde(default)]
     pub height: usize,
 }
--- src/test/unit/infrostructure/camera/camera_test.rs
+++ src/test/unit/infrostructure/camera/camera_test.rs
@@ -4,7 +4,7 @@ mod camera {
     use std::{sync::Once, time::{Duration, Instant}};
     use testing::stuff::max_test_duration::TestDuration;
     use debugging::session::debug_session::{DebugSession, LogLevel, Backtrace};
-    use crate::{domain::dbg::dbgid::DbgId, infrostructure::camera::{camera::Camera, camera_conf::CameraConf}};
+    use crate::{domain::dbg::dbgid::DbgId, infrostructure::camera::{camera::Camera, camera_conf::CameraConf, camera_resolution::CameraResolution}};
     ///
     ///
     static INIT: Once = Once::new();
@@ -30,7 +30,12 @@ mod camera {
         log::debug!("\n{}", dbg);
         let test_duration = TestDuration::new(&dbg, Duration::from_secs(1));
         test_duration.run().unwrap();
-        Camera::new(&dbg, CameraConf {});
+        let resolution: CameraResolution = serde_yaml::from_str(r"
+            width: 1200
+            height: 800
+        ").unwrap();
+        log::debug!("{}.read | resolution: {:?}", dbg, resolution);
+        // Camera::new(&dbg, CameraConf {});
         // assert!(result == target, "step {} \nresult: {:?}\ntarget: {:?}", step, result, target);
         test_duration.exit();
     }
