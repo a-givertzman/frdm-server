4a6e71f9fc3fa4c097fa567b72b6519cef508837
Anton Lobanov <a.givertzman@icloud.com>
2025-03-10 22:09:52 +0300

    Camera | Arena SDK

--- src/test/unit/infrostructure/camera/arena_test.rs
+++ src/test/unit/infrostructure/camera/arena_test.rs
@@ -2,7 +2,7 @@
 
 mod tests {
     use std::{sync::Once, time::{Duration, Instant}};
-    use lucid_arena_sys::{acDeviceGetNodeMap, acOpenSystem, acSystemUpdateDevices};
+    use lucid_arena_sys::{acCloseSystem, acDeviceGetNodeMap, acOpenSystem, acSystemGetNumDevices, acSystemUpdateDevices, AC_ERROR_LIST_AC_ERR_SUCCESS};
     use testing::stuff::max_test_duration::TestDuration;
     use debugging::session::debug_session::{DebugSession, LogLevel, Backtrace};
     use crate::domain::dbg::dbgid::DbgId;
@@ -32,13 +32,38 @@ mod tests {
         let test_duration = TestDuration::new(dbg, Duration::from_secs(1));
         test_duration.run().unwrap();
         unsafe {
-            let phSystem = std::ptr::null_mut();
-            let err = acOpenSystem(phSystem);
-            log::error!("acOpenSystem | err: {}", err);
-            let err = acSystemUpdateDevices(*phSystem, 100);
-            log::error!("acSystemUpdateDevices | err: {}", err);
+            let mut sys = std::mem::zeroed();
+
+            let err = acOpenSystem(&mut sys);
+            log_error("acOpenSystem", err);
+            assert!(err == AC_ERROR_LIST_AC_ERR_SUCCESS);
+
+            let mut num_devices: usize = 0;
+
+            let err = acSystemUpdateDevices(sys, 200);
+            log_error("acSystemUpdateDevices", err);
+            assert!(err == AC_ERROR_LIST_AC_ERR_SUCCESS);
+
+            let err = acSystemGetNumDevices(sys, &mut num_devices);
+            log_error("acSystemGetNumDevices", err);
+            assert!(err == AC_ERROR_LIST_AC_ERR_SUCCESS);
+            log::debug!("Devices detected: {}", num_devices);
+
+            let err = acCloseSystem(sys);
+            log_error("acCloseSystem", err);
+            assert!(err == AC_ERROR_LIST_AC_ERR_SUCCESS);
+
         }
         // assert!(result == target, "step {} \nresult: {:?}\ntarget: {:?}", step, result, target);
         test_duration.exit();
+    }
+    ///
+    /// 
+    fn log_error(dbg: &str, err: i32) {
+        if err > 0 {
+            log::error!("{} | err: {}", dbg, err);
+        } else {
+            log::debug!("{} | Ok", dbg);
+        }
     }
 }
