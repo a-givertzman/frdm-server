9b17373646fb06d0a119c7c3d4532e49abac8a6d
Anton Lobanov <a.givertzman@icloud.com>
2025-07-21 15:59:21 +0300

    DetectingContours | Conf

--- src/conf/conf.rs
+++ src/conf/conf.rs
@@ -1,10 +1,61 @@
+use sal_core::dbg::Dbg;
+use sal_sync::services::{conf::{ConfTree, ConfTreeGet}, entity::Name};
 use crate::conf::{DetectingContoursConf, FastScanConf, FineScanConf};
 
 ///
-/// Te application configuration
+/// The application configuration
+/// 
+/// ### Example
+/// ```yaml
+/// detecting-contours:
+///     gausian:
+///         kernel-size:
+///             width: 3
+///             heidht: 3
+///         sigma-x: 0.0
+///         sigma-y: 0.0
+///     sobel:
+///         kernel-size: 3
+///         scale: 1.0
+///         delta: 0.0
+///     overlay:
+///         src1-weight: 0.5
+///         src2-weight: 0.5
+///         gamma: 0.0
+/// fast-scan:
+///     geometry-defect-threshold: 1.2      # 1.1...1.3
+/// fine-scan:
+///     no-params: not implemented yet
+/// ```
 #[derive(Debug, PartialEq, Clone)]
 pub struct Conf {
     pub detecting_contours: DetectingContoursConf,
     pub fast_scan: FastScanConf,
     pub fine_scan: FineScanConf,
+}
+impl Conf {
+    ///
+    /// Returns [Conf] built from `ConfTree`:
+    pub fn new(parent: impl Into<String>, conf: ConfTree) -> Self {
+        let parent = parent.into();
+        let me = "Conf";
+        let dbg = Dbg::new(&parent, me);
+        log::trace!("{}.new | conf: {:?}", dbg, conf);
+        let name = Name::new(parent, me);
+        log::debug!("{}.new | name: {:?}", dbg, name);
+        let detecting_contours = conf.get("detecting-contours").expect(&format!("{dbg}.new | 'detecting-contours' - not found or wrong configuration"));
+        let detecting_contours = DetectingContoursConf::new(&name, detecting_contours);
+        log::debug!("{dbg}.new | detecting-contours: {:#?}", detecting_contours);
+        let fast_scan = conf.get("fast-scan").expect(&format!("{dbg}.new | 'fast-scan' - not found or wrong configuration"));
+        let fast_scan = FastScanConf::new(&name, fast_scan);
+        log::debug!("{dbg}.new | fast-scan: {:#?}", fast_scan);
+        let fine_scan = conf.get("fine-scan").expect(&format!("{dbg}.new | 'fine-scan' - not found or wrong configuration"));
+        let fine_scan = FineScanConf::new(&name, fine_scan);
+        log::debug!("{dbg}.new | fine-scan: {:#?}", fine_scan);
+        Self {
+            detecting_contours,
+            fast_scan,
+            fine_scan,
+        }
+    }
 }
--- src/conf/fine_scan_conf.rs
+++ src/conf/fine_scan_conf.rs
@@ -1,2 +1,25 @@
+use sal_core::dbg::Dbg;
+use sal_sync::services::{conf::ConfTree, entity::Name};
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct FineScanConf {}
+//
+//
+impl FineScanConf {
+    ///
+    /// Returns [FineScanConf] built from `ConfTree`:
+    pub fn new(parent: impl Into<String>, conf: ConfTree) -> Self {
+        let parent = parent.into();
+        let me = "RopeConf";
+        let dbg = Dbg::new(&parent, me);
+        log::trace!("{}.new | conf: {:?}", dbg, conf);
+        let name = Name::new(parent, me);
+        log::debug!("{}.new | name: {:?}", dbg, name);
+        // let geometry_defect_threshold = conf.get("geometry-defect-threshold").unwrap();
+        // let geometry_defect_threshold = Threshold(geometry_defect_threshold);
+        // log::debug!("{dbg}.new | geometry-defect-threshold: {:?}", geometry_defect_threshold);
+        Self {
+            // geometry_defect_threshold,
+        }
+    }
+}
