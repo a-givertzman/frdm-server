4b9564c1125294aa4deaaf9ec803ed9b7365dbff
Anton Lobanov <a.givertzman@icloud.com>
2025-07-21 15:48:06 +0300

    DetectingContours | Conf

--- src/conf/conf.rs
+++ src/conf/conf.rs
@@ -2,6 +2,7 @@ use crate::conf::{DetectingContoursConf, FastScanConf, FineScanConf};
 
 ///
 /// Te application configuration
+#[derive(Debug, PartialEq, Clone)]
 pub struct Conf {
     pub detecting_contours: DetectingContoursConf,
     pub fast_scan: FastScanConf,
