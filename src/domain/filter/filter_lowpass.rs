9321e633c4033dfe1a75177ded0d73c1675d588e
Anton Lobanov <a.givertzman@icloud.com>
2025-08-11 18:20:17 +0300

    EdgeDetection | Smoothed

--- src/domain/filter/filter_lowpass.rs
+++ src/domain/filter/filter_lowpass.rs
@@ -33,8 +33,8 @@ impl<const N: usize> Filter for FilterLowPass<N, i32> {
     //
     //
     fn add(&mut self, value: Self::Item) -> Option<Self::Item> {
-        let sum = self.buffer.iter().skip(1).sum::<i32>() + value;
-        let average = ((sum as f64) / (self.buffer.len() as f64)).round() as i32;
+        let sum = self.buffer.iter().sum::<i32>() + value;
+        let average = ((sum as f64) / ((self.buffer.len() + 1) as f64)).round() as i32;
         self.buffer.push_back(average);
         self.buffer.front().map(|v| *v)
     }
